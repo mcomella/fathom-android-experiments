41051772609eed365566f52eb907351f34459f63
Michael Comella <michael.l.comella@gmail.com>
2016-08-24 15:19:40 -0700

    inject javascript into page.

--- app/src/main/java/me/mcomella/fathomtest/MainActivity.java
+++ app/src/main/java/me/mcomella/fathomtest/MainActivity.java
@@ -2,9 +2,12 @@ package me.mcomella.fathomtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ValueCallback;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +17,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final WebView webView = (WebView) findViewById(R.id.webview);
-        webView.setWebViewClient(new WebViewClient());
         final WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new InjectClient());
         webView.loadUrl("http://apple.com");
+    }
+
+    public class InjectClient extends WebViewClient {
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+
+            final String script = "document.location = 'http://kotaku.com'";
+
+            view.evaluateJavascript(script, new ValueCallback<String>() {
+                @Override
+                public void onReceiveValue(String s) {
+                    Log.d("lol", "s: " + s);
+                    Toast.makeText(MainActivity.this, "Finished: " + s, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
